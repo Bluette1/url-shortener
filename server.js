51b94ec1bbc6bb2e700c1d4d4e6f7626953bc454
Bluette1 <marylene.sawyer@gmail.com>
2021-11-18 07:42:26 +0200

    Add MongoDB Database connection
    - Create Mongoose schema

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true,
+});
+
+app.use(express.urlencoded({extended: false}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(process.env.PORT || 3000);
+app.post('/shortUrls', async(req, res) => {
+  await ShortUrl.create({full: req.body.fullUrl})
+  res.redirect('/');
+});
+
+mongoose.connection.on('open', () => {
+  app.listen(process.env.PORT || 3000);
+});
